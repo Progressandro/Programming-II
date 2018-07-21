a3dc6d428562d3cd2eecdb05f3aebc5b4e80acf6
Alejandro Fabrega <progressandro@gmail.com>
2018-07-21 12:42:19 -0400

    +Started vagones.cpp

--- LinearStructures/Practice/Vagones/vagones.cpp
+++ LinearStructures/Practice/Vagones/vagones.cpp
@@ -2,21 +2,30 @@
 #include "Stack.hpp"
 #include "List.hpp"
 
+void reorganizable(List<int> vagones);
+
 int main() {
-  Stack<int> vagones;
+  List<int> vagones;
   int numVagones;
   
   while (cin >> numVagones && numVagones) {
     int vagon;
     while (cin >> vagon && vagon) {
-      vagones.push(vagon);
+      vagones.insert(vagon, 1);
       for (int i = 1; i<numVagones; i++) {
         cin >> vagon;
-        vagones.push(vagon);
+        vagones.insert(vagon, 1);
       }
-      vagones.print();
+      vagones.reverse();
+      reorganizable(vagones);
       vagones.flush();
     }
   }
   return 0;
+}
+
+void reorganizable(List<int> vagones) {
+  for(int i = 0; i < vagones.lenght(); i++) {
+    
+  }
 }
