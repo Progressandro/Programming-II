cd97cfafc10234ae42d1f5014bfc882185ea567b
Alejandro Fabrega <progressandro@gmail.com>
2018-11-05 13:15:21 -0400

    +Finished NodeBT.hpp

--- Trees/BinaryTree/NodeBT.hpp
+++ Trees/BinaryTree/NodeBT.hpp
@@ -12,21 +12,23 @@ private:
   NodeBT<T>* _right = NULL;
 
 public:
+  NodeBT();
   NodeBT(T data) {this->data = data;}
   ~NodeBT();
 
-  bool getData() {return this->data;}
+  T getData() {return this->data;}
   void setData(T data) {this->data = data;}
-  T getLeft() {return this->_left;}
-  void setLeft(T data) {this->_left = data;}
-  T getRight() {return this->_right;}
-  void setRight(T data) {this->_right = data;}
+  NodeBT<T>* getLeft() {return this->_left;}
+  void setLeft(NodeBT<T>* _newLeft) {this->_left = _newLeft;}
+  NodeBT<T>* getRight() {return this->_right;}
+  void setRight(NodeBT<T>* _newRight) {this->_right = _newRight;}
   NodeBT<T>* copy(NodeBT<T>* _originalNode);
   NodeBT<T>& operator=(const NodeBT<T>& originalList);
-  void printNode(NodeBT<T>* _node);
-
 };
+template <typename T>
+NodeBT<T>::NodeBT() {
 
+}
 template <typename T>
 NodeBT<T>* NodeBT<T>::copy(NodeBT<T>* _originalNode) {
   NodeBT<T>* _root = new NodeBT<T>;
@@ -46,14 +48,6 @@ NodeBT<T>& NodeBT<T>::operator=(const NodeBT<T>& originalNode) {
   _returnedNode = copy(*originalNode);
 
   return _returnedNode;
-}
-
-template <typename T>
-void NodeBT<T>::printNode(NodeBT<T>* _node) {
-  if (!_node) return;
-  if (_node->left) printNode(_node->left);
-  if (_node->right) printNode(_node->right);
-  cout << _node->getData() << " ";
 }
 
 #endif
